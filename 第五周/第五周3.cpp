e8e1c2b1086a10dd70d6164707b818c561cde6be
song-song797 <2276071652@qq.com>
2026-04-01 17:53:06 +0800

    第五周第3题

--- 第五周/第五周3.cpp
+++ 第五周/第五周3.cpp
@@ -1,13 +1,26 @@
 // Vjudge 第五周 3
 // TODO: 在这里补充题目描述和代码实现。
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // TODO: 实现题目逻辑
-
+    int N;
+    cin >> N;
+    vector<int> T(N + 1);
+    int S = 0;
+    for (int i = 1; i <= N; i++) {
+        cin >> T[i];
+        S += T[i];
+    }
+    vector<int> dp(S / 2 + 1, 0);
+    for (int i = 1; i <= N; i++) {
+        for (int j = S / 2; j >= T[i]; j--) {
+            dp[j] = max(dp[j], dp[j - T[i]] + T[i]);
+        }
+    }
+    cout << S - dp[S / 2] << '\n';
 }
