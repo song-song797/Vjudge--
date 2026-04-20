9e2227b6902c6f85a294ef739c77219a1b4f601b
wjs <2276071652@qq.com>
2026-04-21 07:56:11 +0800

    no message

--- 第七周/第七周8.cpp
+++ 第七周/第七周8.cpp
@@ -7,5 +7,11 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int N;
+    cin >> N;
+    vector<pair<int, int>> menu(N + 1);
+    for (int i = 1; i <= N; i++) {
+        cin >> menu[i].first >> menu[i].second;
+    }
     return 0;
 }
