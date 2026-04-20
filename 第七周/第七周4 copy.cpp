863caded568afe5e4ff72155121028741ce77742
wjs666-admin <wangjinsong0523@gmail.com>
2026-04-20 13:28:19 +0800

    no message

--- 第七周/第七周4 copy.cpp
+++ 第七周/第七周4 copy.cpp
@@ -1,5 +1,3 @@
-// Vjudge 第七周4
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -29,66 +27,68 @@ int main() {
         cin >> paperX[i];
     }
 
-    for (int rowX = 0; rowX < HX; rowX++) {
-        for (int colX = 0; colX < WX; colX++) {
-            for (int i = 0; i < 15; i++)
-                fill(visited[i], visited[i] + 15, false);
-            for (int rowA = 0; rowA < HA; rowA++) {
-                for (int colA = 0; colA < WA; colA++) {
-                    if (paperX[rowX][colX] != paperA[rowA][colA])
+    for (int shiftA_row = -10; shiftA_row <= 10; shiftA_row++) {
+        for (int shiftA_col = -10; shiftA_col <= 10; shiftA_col++) {
+            for (int shiftB_row = -10; shiftB_row <= 10; shiftB_row++) {
+                for (int shiftB_col = -10; shiftB_col <= 10; shiftB_col++) {
+                    vector<string> C(HX, string(WX, '.'));
+                    bool valid = true;
+
+                    for (int i = 0; i < HA; i++) {
+                        for (int j = 0; j < WA; j++) {
+                            if (paperA[i][j] == '#') {
+                                int r = i + shiftA_row;
+                                int c = j + shiftA_col;
+                                if (r >= 0 && r < HX && c >= 0 && c < WX) {
+                                    C[r][c] = '#';
+                                } else {
+                                    valid = false;
+                                }
+                            }
+                        }
+                    }
+
+                    if (!valid)
                         continue;
-                    for (int row_offset = 0; row_offset + rowX < HX; row_offset++) {
-                        for (int col_offset = 0; col_offset + colX < WX; col_offset++) {
-                            if (row_offset + rowA < HA && col_offset + colA < WA) {
-                                if (paperX[rowX + row_offset][colX + col_offset] == paperA[rowA + row_offset][colA + col_offset]) {
-                                    visited[rowX + row_offset][colX + col_offset] = true;
-                                }
-                            } else {
-                                if (paperX[rowX + row_offset][colX + col_offset] == '.') {
-                                    visited[rowX + row_offset][colX + col_offset] = true;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            for (int rowX1 = 0; rowX1 < HX; rowX1++) {
-                for (int colX1 = 0; colX1 < WX; colX1++) {
-                    for (int rowB = 0; rowB < HB; rowB++) {
-                        for (int colB = 0; colB < WB; colB++) {
-                            if (paperX[rowX1][colX1] != paperB[rowB][colB])
-                                continue;
-                            for (int row_offset = 0; row_offset + rowX1 < HX && row_offset + rowB < HB; row_offset++) {
-                                for (int col_offset = 0; col_offset + colX1 < WX && col_offset + colB < WB; col_offset++) {
-                                    if (row_offset + rowB < HB && col_offset + colB < WB) {
-                                        if (paperX[rowX1 + row_offset][colX1 + col_offset] == paperB[rowB + row_offset][colB + col_offset]) {
-                                            visited[rowX1 + row_offset][colX1 + col_offset] = true;
-                                        }
-                                    } else {
-                                        if (paperX[rowX1 + row_offset][colX1 + col_offset] == '.') {
-                                            visited[rowX1 + row_offset][colX1 + col_offset] = true;
-                                        }
-                                    }
+
+                    for (int i = 0; i < HB; i++) {
+                        for (int j = 0; j < WB; j++) {
+                            if (paperB[i][j] == '#') {
+                                int r = i + shiftB_row;
+                                int c = j + shiftB_col;
+                                if (r >= 0 && r < HX && c >= 0 && c < WX) {
+                                    C[r][c] = '#';
+                                } else {
+                                    valid = false;
                                 }
                             }
                         }
                     }
-                }
-            }
-            bool valid = true;
-            for (int i = 0; i < HX; i++) {
-                for (int j = 0; j < WX; j++) {
-                    if (!visited[i][j]) {
-                        valid = false;
+
+                    if (!valid)
+                        continue;
+
+                    bool match = true;
+                    for (int i = 0; i < HX; i++) {
+                        for (int j = 0; j < WX; j++) {
+                            if (C[i][j] != paperX[i][j]) {
+                                match = false;
+                                break;
+                            }
+                        }
+                        if (!match)
+                            break;
+                    }
+
+                    if (match) {
+                        cout << "Yes\n";
+                        return 0;
                     }
                 }
-            }
-            if (valid) {
-                cout << "Yes" << '\n';
-                return 0;
             }
         }
     }
-    cout << "No" << '\n';
+
+    cout << "No\n";
     return 0;
 }
