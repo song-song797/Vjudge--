69e59f40e36ab7541d430bd3f7eaffaf5da6818e
wjs666-admin <wangjinsong0523@gmail.com>
2026-04-21 12:47:12 +0800

    Update week 7 solutions

--- 第七周/第七周3.cpp
+++ 第七周/第七周3.cpp
@@ -3,9 +3,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int counts[300005];
+vector<vector<int>> adj;
+void dfs(int idx,int remain){
+    for(auto v:adj[idx]){
+        counts[v]=max(remain-1,counts[v]);
+        dfs(v,counts[v]);
+    }
+}
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int N,M;
+    cin >> N >> M;
+ 
+    adj.resize(N+1);
+    for(int i=2;i<=N;i++){
+        int p;
+        cin >> p;
+        adj[p].push_back(i);
+    }
+    
+    for(int i=1;i<=M;i++){
+        int x,y;
+        cin >> x >> y;
+        if(counts[x]<y+1){
+            counts[x]=y+1;
+        }
+    }
+   
+    dfs(1,counts[1]);
+    
+    int res=0;
+    for(int i=1;i<=N;i++){
+        if(counts[i]>0) res++;
+    }
+    cout << res << '\n';
     return 0;
 }
--- 第七周/第七周8.cpp
+++ 第七周/第七周8.cpp
@@ -2,8 +2,8 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
+#define int long long
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -13,5 +13,20 @@ int main() {
     for (int i = 1; i <= N; i++) {
         cin >> menu[i].first >> menu[i].second;
     }
+
+    vector<vector<int>> dp(N + 1, vector<int>(2, -1e18));
+    dp[0][0] = 0;
+    for (int i = 1; i <= N; i++) {
+        dp[i][0] = max(dp[i][0], dp[i - 1][0]);
+        dp[i][1] = max(dp[i][1], dp[i - 1][1]);
+        if (menu[i].first == 0) {
+            dp[i][0] = max(dp[i][0], dp[i - 1][1] + menu[i].second);
+            dp[i][0] = max(dp[i][0], dp[i - 1][0] + menu[i].second);
+        } else {
+            dp[i][1] = max(dp[i][1], dp[i - 1][0] + menu[i].second);
+        }
+    }
+
+    cout << max(dp[N][0], dp[N][1]) << '\n';
     return 0;
 }
